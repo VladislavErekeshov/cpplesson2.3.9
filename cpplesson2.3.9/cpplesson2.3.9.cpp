113bc43a8a06ccb1136c31ca94d41007e9a15999
VladislavErekeshov <tommysyching@gmail.com>
2021-02-20 22:06:21 +0300

    Добавьте файлы проекта.

---
+++ cpplesson2.3.9/cpplesson2.3.9.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, d, e, ans1, ans2, ans3, ans4, ans5;
+    cin >> a >> b >> c >> d >> e;
+    ans1 = a % 2;
+    ans2 = b % 2;
+    ans3 = c % 2;
+    ans4 = d % 2;
+    ans5 = e % 2;
+    cout << ans1 << ans2 << ans3 << ans4 << ans5;
+    return 0;
+}
